8512f1cd78c530a02c308833a93c7b1d9653b7a9
Igor Gebauer Garcia <121063329+Igor2GG@users.noreply.github.com>
2024-02-15 22:31:03 +0000

    Revert "Update 360"
    This reverts commit bc178c0f9cf745b3aafae3b6219ab4f5e51f8461.

---
+++ ProyectoClase_IGG/Assets/ProyectoIGG/Scripts/SC_CambiadorDeNave.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class SC_CambiadorDeNave : MonoBehaviour
+{
+
+    public GameObject[] prefabsDeNaves; // Array público de prefabs de naves que podemos asignar desde el editor de Unity
+    private Transform  puntoDeSpawn; // Variable privada para almacenar la referencia al punto de spawn en la escena
+
+
+    void Start()
+    {
+        // Busca en la escena el objeto llamado "PuntoSpawn" y guardamos su transform
+        puntoDeSpawn = GameObject.Find("PuntoSpawn").transform;
+
+        CambiarNave();
+    }
+
+    // Método para cambiar la nave basado en la selección guardada
+    public void CambiarNave()
+    {
+        // Leemos el índice de la nave seleccionada de PlayerPrefs, 0 es el valor por defecto si no se encuentra nada
+        int indiceSeleccionado = PlayerPrefs.GetInt("NaveSeleccionada", 0); 
+
+        // Verifica si ya existe una nave y destrúyela
+        if (puntoDeSpawn != null && puntoDeSpawn.gameObject != null)
+        {
+            Destroy(puntoDeSpawn.gameObject);
+        }
+
+        // Instanciar la nueva nave basada en la selección del dropdown
+        if (indiceSeleccionado >= 0 && indiceSeleccionado < prefabsDeNaves.Length)
+        {
+            // Instanciamos la nueva nave en la posición y rotación del punto de spawn
+            GameObject nuevaNave = Instantiate(prefabsDeNaves[indiceSeleccionado], puntoDeSpawn.position, puntoDeSpawn.rotation);
+            nuevaNave.transform.SetParent(puntoDeSpawn.parent); // Hacemos que la nueva nave sea hija del punto de spawn para mantener la jerarquía organizada
+        }
+    }
+}
