f581488956294a14c747d9d7cdc0c5a60b085ce6
Igor Gebauer Garcia <121063329+Igor2GG@users.noreply.github.com>
2024-02-26 12:18:12 +0000

    Update SC_Vida.cs
    Ya no reinicia automaticamente, se para el juego y te deja elegir en la interfaz.

--- ProyectoClase_IGG/Assets/Actores/NAves/SC_Vida.cs
+++ ProyectoClase_IGG/Assets/Actores/NAves/SC_Vida.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.UI; // Necesario para UI como Image
 using UnityEngine.SceneManagement; // Necesario para manejar cambios de escena
-using System.Diagnostics;
 
 public class SC_Vida : MonoBehaviour
 {
@@ -10,8 +9,8 @@ public class SC_Vida : MonoBehaviour
     public int vida; // Representa la vida actual del jugador (3 corazones)
     public bool invencible = false; // Indica si el jugador es temporalmente invencible (cuando recibe daño)
     public float tiempoInvencible = 1f; // Tiempo durante el cual el jugador es invencible despues de recibir daño
-    private bool win;
-    private bool muerto;
+    public bool win;
+    public bool muerto;
 
     // Prefab de las particulas de explosiones
     [SerializeField] private GameObject particulaExplosionPrefab;
@@ -19,8 +18,19 @@ public class SC_Vida : MonoBehaviour
     // Componente Rigidbody2D para manejar la fisica
     private Rigidbody2D rb2d;
 
+    // Para setear la puntuacion al colisionar.
+    private SC_Puntuacion scPuntuacion;
+
+    private SC_FadeOut fadeout;
+    
+
     private void Start()
     {
+        //Referenciamos la nave, luego a su componente script.
+        GameObject nave = GameObject.FindGameObjectWithTag("HUD");
+        scPuntuacion= nave.GetComponent<SC_Puntuacion>();
+
+
         // Encuentra el panel que contiene los corazones de la UI y obtiene todas las imagenes
         GameObject panelCorazonesObj = GameObject.Find("PanelDeVida");
         corazones = panelCorazonesObj.GetComponentsInChildren<Image>();
@@ -28,6 +38,18 @@ public class SC_Vida : MonoBehaviour
 
         // Obtiene el componente Rigidbody2D para fisicas
         rb2d = GetComponent<Rigidbody2D>();
+
+        // Funciona para llamar al jodio fade, menos royo.
+        fadeout = GameObject.FindGameObjectWithTag("Fade").GetComponent<SC_FadeOut>();
+    }
+
+    void Update()
+    {
+        if (!win)
+        {
+            scPuntuacion.CalcularYMostrarPuntuacion();
+
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -38,15 +60,22 @@ public class SC_Vida : MonoBehaviour
         // Manejo de colision con objetos con tag Finish
         if (other.gameObject.CompareTag("Finish"))
         {
+            
+
             musicController.PlayVictoryMusic(); // Reproduce musica de victoria
             win = true;
             DetenerNave();
-            StartCoroutine(ReiniciarJuegoConDelay(4.0f)); // Espera antes de reiniciar el juego o cargar siguiente nivel
+            //StartCoroutine(ReiniciarJuegoConDelay(4.0f)); // Espera antes de reiniciar el juego o cargar siguiente nivel
+            
+            Debug.Log("Pasando fade");
+            fadeout.Fade(); // Fade  
         }
         // Manejo de colision con objetos con tag Muerte
         else if (other.gameObject.CompareTag("Muerte"))
         {
             ProcesarMuerte(musicController);
+            scPuntuacion.puntuacion=0;
+
         }
         // Otros casos de colision que tambiwn son muerte
         else if (!other.gameObject.CompareTag("Finish") && !other.gameObject.CompareTag("NON"))
@@ -59,6 +88,7 @@ public class SC_Vida : MonoBehaviour
     {
         yield return new WaitForSeconds(delaySeconds);
         SceneManager.LoadScene(win ? SceneManager.GetActiveScene().buildIndex + 1 : SceneManager.GetActiveScene().buildIndex);
+        
     }
 
     void Explota()
@@ -128,7 +158,9 @@ public class SC_Vida : MonoBehaviour
             Explota();
             musicController.PlayExplosionSound();
             win = false;
-            StartCoroutine(ReiniciarJuegoConDelay(4.0f));
+            //StartCoroutine(ReiniciarJuegoConDelay(4.0f));
+
+            fadeout.Fade(); // Fade
         }
     }
 
